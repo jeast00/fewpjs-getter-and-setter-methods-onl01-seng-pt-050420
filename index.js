428a462c0fdd628cb78ddf31d01ede090b93935f
Jonathan East <jonathan_east@ymail.com>
2020-12-12 16:21:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,33 @@ class Circle {
   constructor(radius) {
     this.radius = radius;
   }
+  
+  get diameter() {
+    
+  }
+  
+  get circumference() {
+    
+  }
+  
+  get area() {
+    
+  }
+  
+  set diameter() {
+    
+  }
+  
+  set circumference() {
+    
+  }  
+  
+  set diameter() {
+    
+  }
 }
+
+
+
+
+
